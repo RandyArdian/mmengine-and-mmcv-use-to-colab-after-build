5b41ab47bbea332cd0f645ece460c7a3032842dd
RandyArdian <112707872+RandyArdian@users.noreply.github.com>
2025-10-23 22:14:59 +0800

    Create mmengine dan mmcv.py

---
+++ mmengine dan mmcv.py
@@ -0,0 +1,27 @@
+#masuk ke drive
+from google.colab import drive
+drive.flush_and_unmount()  # lepas
+drive.mount('/content/gdrive', force_remount=True)
+
+#masuk ke folder
+%cd gdrive
+%cd MyDrive/Riset
+
+#karena sudah build maka tinggal buka lagi
+#untuk mmengine
+%cd mmengine
+!pip install -e . -v
+
+#untuk cv
+%cd ..
+%cd mmcv
+!pip install -e .
+
+#kedua proses tersebut sebentar
+#langsung bisa pake librarynya
+from mmengine.model import constant_init
+from mmcv.cnn import build_activation_layer, build_norm_layer
+from mmcv.ops.modulated_deform_conv import ModulatedDeformConv2d
+
+#kedua library tersebut bisa dipanggil di modul ultralytics, sepeti contoh diatas.
+#Randy_Ardiansyah_23_oktober_2025
